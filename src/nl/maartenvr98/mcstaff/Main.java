1939b042033435b56e59f4310317eaa4b5009ba6
Maarten van Rijssel <maartenrijssel@hotmail.com>
2017-11-08 10:59:18 +0100

    Create config. Insert/update player in database

--- src/nl/maartenvr98/mcstaff/Main.java
+++ src/nl/maartenvr98/mcstaff/Main.java
@@ -1,5 +1,6 @@
 package nl.maartenvr98.mcstaff;
 
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,14 +8,14 @@ import java.sql.Statement;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import javax.swing.plaf.nimbus.State;
 
 public class Main extends JavaPlugin implements Listener {
 
@@ -25,16 +26,27 @@ public class Main extends JavaPlugin implements Listener {
     private String password;
     private int port;
 
+    private FileConfiguration config = getConfig();
+
     @Override
     public void onEnable() {
+        config.addDefault("enabled", true);
+        config.addDefault("database.host", "80.82.222.241");
+        config.addDefault("database.port", 3306);
+        config.addDefault("database.username", "java");
+        config.addDefault("database.password", "bHts0~06");
+        config.addDefault("database.database", "test_java");
+        config.options().copyDefaults(true);
+        saveConfig();
+
         System.out.println("Plugin enabled");
         this.getServer().getPluginManager().registerEvents(this, this);
 
-        this.host = "80.82.222.241";
-        this.port = 3306;
-        this.database = "test_java";
-        this.username = "java";
-        this.password = "bHts0~06";
+        this.host = config.getString("database.host");
+        this.port = config.getInt("database.post");
+        this.database = config.getString("database.database");
+        this.username = config.getString("database.username");
+        this.password = config.getString("database.password");
     }
 
     @Override
@@ -44,17 +56,23 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        if(!config.getBoolean("enabled")) {
+            return;
+        }
         Player p = event.getPlayer();
 
         try {
             this.openConnection();
             Statement statement = this.connection.createStatement();
 
+            DateFormat fullFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
             DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
             DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
             Date date = new Date();
 
-            //TODO: Insert or update if exsist player in players table
+            statement.executeUpdate("INSERT INTO players (name, uuid, lastlogin, lastip) " +
+                    "VALUES ('" + p.getName() + "', '" + p.getUniqueId().toString() + "', '" + fullFormat.format(date) + "', '" + p.getAddress().getHostName() + "') " +
+                    "ON DUPLICATE KEY UPDATE lastlogin = values(lastlogin), lastip = values(lastip)");
 
             statement.executeUpdate("INSERT INTO players_events (uuid, type, date, time) VALUES ('" + p.getUniqueId().toString() + "', 'join', '" + dateFormat.format(date) + "', '" + timeFormat.format(date) + "');");
 
@@ -71,6 +89,9 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
+        if(!config.getBoolean("enabled")) {
+            return;
+        }
         Player p = event.getPlayer();
 
         try {
